7d2e07f1c2637f19b47c533eb485846a13838c21
Valerie Kraucunas <valeriekraucunas@gmail.com>
2016-01-18 19:01:41 -0700

    initial commit 1.18 warmup complete

---
+++ warmup18.js
@@ -0,0 +1,23 @@
+var random = Math.floor((Math.random()*100)+1);
+console.log(random);
+
+var userGuess;
+
+var buttonClick = document.getElementById('submitButton');
+
+function game (num, guess) {
+  if (guess < num) {
+    alert("Your guess is too low! Try again");
+  } else if (guess > num) {
+    alert("Your guess is too high! Try again");
+  } else {
+    alert("You're correct!!");
+  }
+}
+
+
+buttonClick.addEventListener('click', function() {
+  userGuess = document.getElementById('userInput').value;
+  console.log("userGuess is", userGuess);
+  game(random, userGuess);
+});
