57e1d885733eba4adc3b8f575ffa0df0e6ab1069
Valerie Kraucunas <valeriekraucunas@gmail.com>
2016-02-23 10:35:01 -0700

    cleaned up today's

--- utility223.js
+++ utility223.js
@@ -1,44 +1,28 @@
-function flatten(arr) {
-  return arr.reduce(function (row, toFlatten) {
-    return row.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
-  }, []);
-}
+// function flatten(arr) {
+//   return arr.reduce(function (row, toFlatten) {
+//     return row.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+//   }, []);
+// }
 
-var test = flatten([
-    [1,2,3],
-    [4,5,6],
-    [8,9,10]
-    ]);
+// var test = flatten([
+//     [1,2,3],
+//     [4,5,6],
+//     [8,9,10]
+//     ]);
 
-console.log(test);
-
-
-module.export = flatten;
+// console.log(test);
 
 
+// module.export = flatten;
 
 
 
-//Mike solution
-// warmup223.js
-var sum = require('./utility');
-
-var result = sum([1,3], function(err, data) {
-    if (err) {
-        return err;
-    } else {
-        return data;
-    }
-});
 
 
-console.log(result);
 
-
-///utility.js
 function sum(arr, callback) {
     if (!Array.isArray(arr)) {
-        return callback("dumb ass");
+        return callback("dumb ass", null);
     } else {
         var results = arr.forEach(function(num) {
             return num + num;
